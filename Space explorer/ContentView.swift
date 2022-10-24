97cb2593b61d1e338c92b11c33bc0706bda86109
Kirill Ki <kirillkalapov@gmail.com>
2022-10-24 13:29:08 +0200

    Initial Commit

---
+++ Space explorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Space explorer
+//
+//  Created by  Mr.Ki on 24.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Space explorer/Space_explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_explorerApp.swift
+//  Space explorer
+//
+//  Created by  Mr.Ki on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Space_explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
